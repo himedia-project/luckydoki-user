e6317280b52cfaf93f87d84a068b1f3afe4dff08
Zara8170 <wlsgusan123@gmail.com>
2025-02-19 17:44:16 +0900

    ⚡️ aiSuggestPage 기본 레이아웃 구성, dropdown 예외처리 추가

--- src/components/dropdown/MessageDropdown.js
+++ src/components/dropdown/MessageDropdown.js
@@ -1,13 +1,27 @@
 import React from "react";
 import styles from "../../styles/Dropdown.module.css";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const MessageDropdown = ({ messages }) => {
   const navigate = useNavigate();
+  const email = useSelector((state) => state.loginSlice.email);
 
   const handleClick = () => {
     navigate("/messages");
   };
+
+  if (!email) {
+    return (
+      <div className={styles.dropdown}>
+        <h3 className={styles.title}>메시지</h3>
+        <div className={styles.empty}>
+          <img src="/clover.png" alt="비어 있음" className={styles.emptyIcon} />
+          <p>로그인이 필요합니다.</p>
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.dropdown}>
@@ -37,11 +51,10 @@ const MessageDropdown = ({ messages }) => {
           <p>메시지가 없습니다.</p>
         </div>
       )}
-      {messages.length > 0 && (
-        <button className={styles.viewAll} onClick={handleClick}>
-          모두 보기
-        </button>
-      )}
+
+      <button className={styles.viewAll} onClick={handleClick}>
+        모두 보기
+      </button>
     </div>
   );
 };
