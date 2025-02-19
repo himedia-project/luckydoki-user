ddbb18732041a2684024e35b20cbbddc587d7f6f
Lo5er <jooys98@naver.com>
2025-02-19 16:09:00 +0900

    chat test

---
+++ src/api/ChatApi.js
@@ -0,0 +1,21 @@
+import axiosInstance from "./axiosInstance";
+
+export const getMessageHistory = async (roomId) => {
+  return await axiosInstance.get(`/chat/history/${roomId}`);
+};
+
+export const createChattingRoom = async (chatRoomData) => {
+  return await axiosInstance.post(`/chat`, {
+    id: chatRoomData.id,
+    member: chatRoomData.member,
+    shopId: chatRoomData.shopId,
+    shopImage: chatRoomData.shopImage,
+    shopName: chatRoomData.shopName,
+    createdAt: new Date().toISOString(), // 현재 시간을 ISO 문자열로 변환
+  });
+};
+
+export const getChatRooms = async () => {
+  console.log("API 호출 시 헤더:", axiosInstance.defaults.headers);
+  return await axiosInstance.get(`/chat/history`);
+};
