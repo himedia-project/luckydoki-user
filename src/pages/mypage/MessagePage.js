9b8940a3d110b1634cd953549ef43f82344dce72
seonggon do <mooh2jj@naver.com>
2025-02-28 21:23:06 +0900

    ⚡️ 타임 함수 위치 수정

--- src/pages/mypage/MessagePage.js
+++ src/pages/mypage/MessagePage.js
@@ -39,6 +39,17 @@ export default function MessagePage() {
 
   const [dropdownMessages, setDropdownMessages] = useState([]);
 
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    const hours = ("0" + date.getHours()).slice(-2);
+    const minutes = ("0" + date.getMinutes()).slice(-2);
+    const seconds = ("0" + date.getSeconds()).slice(-2);
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
   // 알림 권한 요청 useEffect
   useEffect(() => {
     if (Notification.permission === "default") {
@@ -300,16 +311,6 @@ export default function MessagePage() {
       }
     }
   };
-  function formatDate(date) {
-    const year = date.getFullYear();
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const day = ("0" + date.getDate()).slice(-2);
-    const hours = ("0" + date.getHours()).slice(-2);
-    const minutes = ("0" + date.getMinutes()).slice(-2);
-    const seconds = ("0" + date.getSeconds()).slice(-2);
-
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-  }
 
   //메세지 전송 함수(connect 함수 호출 필요)
   const sendMessage = async (e) => {
