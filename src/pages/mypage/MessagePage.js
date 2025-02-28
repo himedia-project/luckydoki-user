f2de42c1bf142aaca311e7462a836e0992b694c4
seonggon do <mooh2jj@naver.com>
2025-02-28 21:47:49 +0900

    다시 시간 포멧

--- src/pages/mypage/MessagePage.js
+++ src/pages/mypage/MessagePage.js
@@ -39,6 +39,19 @@ export default function MessagePage() {
 
   const [dropdownMessages, setDropdownMessages] = useState([]);
 
+  // 날짜 포맷팅 함수
+  // 2025-02-28 11:35:34
+  const formatData = (date) => {
+    const year = date.getFullYear();
+    const month = ("0" + (date.getMonth() + 1)).slice(-2);
+    const day = ("0" + date.getDate()).slice(-2);
+    const hours = ("0" + date.getHours()).slice(-2);
+    const minutes = ("0" + date.getMinutes()).slice(-2);
+    const seconds = ("0" + date.getSeconds()).slice(-2);
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  };
+
   // 알림 권한 요청 useEffect
   useEffect(() => {
     if (Notification.permission === "default") {
@@ -363,7 +376,7 @@ export default function MessagePage() {
 
       // const currentTime = new Date().toISOString();
       // "2025-02-28 11:35:34"
-      const currentTime = new Date().toLocaleString();
+      const currentTime = formatData(new Date());
       const chatMessage = {
         roomId: roomId || selectedRoom?.id,
         sender: null,
