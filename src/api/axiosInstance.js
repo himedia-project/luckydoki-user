f7a7d5f5b9d953d1a062125dd2ca4855f554a61d
seonggon do <mooh2jj@naver.com>
2025-03-20 19:54:03 +0900

    ⚡️ 리프레쉬토큰 만료시 axiosInstance 내에서 처리하기

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,17 @@
 import axios from "axios";
 import store from "./redux/store";
-import { login, setAccessToken } from "./redux/loginSlice";
+import { login, logout } from "./redux/loginSlice";
 import { API_URL } from "../config/apiConfig";
-import { useDispatch } from "react-redux";
+import Swal from "sweetalert2";
+import { clearCartItems, setCartEmail } from "./redux/cartSlice";
+import {
+  clearNotificationItems,
+  setNotificationEmail,
+} from "./redux/notificationSlice";
+import { setMessageEmail } from "./redux/messageSlice";
+import { clearInfo } from "./redux/infoSlice";
+import { logoutPost } from "./loginApi";
+
 const axiosInstance = axios.create({
   baseURL: `${API_URL}/api`,
   // 쿠키 허용
@@ -43,22 +52,52 @@ axiosInstance.interceptors.response.use(
       error.response.data &&
       error.response.data.error === "ERROR_ACCESS_TOKEN" // TODO: 토큰 만료 시에만! 토큰 갱신
     ) {
-      const result = await refreshJWT();
-      console.log("refreshJWT RESULT", result);
+      try {
+        const result = await refreshJWT();
+        console.log("refreshJWT RESULT", result);
 
-      // 로그인 성공 시 Redux store 업데이트
-      // 로그인 성공 시 Redux store 업데이트
-      // interceptor에서는 직접 store.dispatch를 사용해야
-      // useDispatch(
-      store.dispatch(
-        login({
-          email: result.email,
-          roles: result.roles,
-          accessToken: result.accessToken,
-        })
-      );
+        // 로그인 성공 시 Redux store 업데이트
+        // 로그인 성공 시 Redux store 업데이트
+        // interceptor에서는 직접 store.dispatch를 사용해야
+        // useDispatch(
+        store.dispatch(
+          login({
+            email: result.email,
+            roles: result.roles,
+            accessToken: result.accessToken,
+          })
+        );
 
-      return axiosInstance(error.config); // 재요청
+        return axiosInstance(error.config); // 재요청
+      } catch (refreshError) {
+        // 리프레시 토큰이 만료되었거나 찾을 수 없는 경우
+        if (refreshError.response?.data?.error === "REFRESH_TOKEN_NOT_FOUND") {
+          // 로그아웃 처리
+          store.dispatch(logout());
+          store.dispatch(setCartEmail(""));
+          store.dispatch(setNotificationEmail(""));
+          store.dispatch(setMessageEmail(""));
+          store.dispatch(clearCartItems());
+          store.dispatch(clearNotificationItems());
+          store.dispatch(clearInfo());
+
+          const response = logoutPost();
+          console.log("logoutPost response: ", response);
+
+          // 알림창 표시
+          await Swal.fire({
+            title: "세션이 만료되었습니다.",
+            text: "다시 로그인해주세요.",
+            icon: "warning",
+            confirmButtonText: "확인",
+            confirmButtonColor: "#00de90",
+          });
+
+          // 로그인 페이지로 리다이렉션
+          window.location.href = "/login";
+        }
+        return Promise.reject(refreshError);
+      }
     }
     return Promise.reject(error);
   }
