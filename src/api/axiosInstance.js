097d73bf87e65ff9120bfc34eda95a75579e2651
seonggon do <mooh2jj@naver.com>
2025-05-02 23:29:05 +0900

    🚀 로그인 리다이렉션 경로를 수정하여 홈으로 변경

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -94,7 +94,7 @@ axiosInstance.interceptors.response.use(
           });
 
           // 로그인 페이지로 리다이렉션
-          window.location.href = "/login";
+          window.location.href = "/";
         }
         return Promise.reject(refreshError);
       }
